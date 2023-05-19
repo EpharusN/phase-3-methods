29ecbf3c6e186b5db6b64ba8cf36ee228f6768e7
Epharus Ndumia <epharusndumia@gmail.com>
2023-05-19 08:22:42 +0300

    initial commit

--- methods.rb
+++ methods.rb
@@ -1,1 +1,51 @@
 # Your code here!
+def greet_programmer
+    "Hello, programmer!"
+    puts "Hello, programmer!"
+    
+end
+
+greet_programmer
+
+def greet(name= "Naureen")
+    puts "Hello, #{name}!"
+end
+
+greet
+
+greet "Jimmy"
+
+
+def greet_with_default(name="programmer")
+
+puts "Hello, #{name}!"
+    
+end
+
+greet_with_default
+
+greet_with_default "Naureen"
+
+
+def add(num1,num2)
+    return num1 + num2
+    
+end
+sum1 = add(1,2)
+
+
+
+def halve(number)
+    if !number.is_a?(Integer)
+      return nil
+    end
+    return number / 2
+  end
+  
+
+
+
+  
+
+
+
